868b0dd3ae1ab3fe732d7f0c50efc9fac4cb24d2
abh1sh3kk <abhishekacharya171@gmail.com>
2023-07-01 14:49:02 +0545

    update origin for cors

--- server/index.ts
+++ server/index.ts
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://xd-noteflow.netlify.app", credentials: true }));
 app.use(cookieParser());
 app.use("/users", userRouter);
 app.use("/notes", noteRouter);
