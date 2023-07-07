d36c068d14978eb7b9634fc1f1496720895dc9d9
abh1sh3kk <abhishekacharya171@gmail.com>
2023-07-07 22:28:52 +0545

    change rate limit

--- server/configs/ratelimit.ts
+++ server/configs/ratelimit.ts
@@ -2,7 +2,7 @@ import { rateLimit } from "express-rate-limit";
 
 export const limiter = rateLimit({
     windowMs: 1000,
-    max: 1,
+    max: 5,
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
