03be4a0cdaf08ea7f23c36ca682b9b6dff881c4c
abh1sh3kk <abhishekacharya171@gmail.com>
2024-08-18 08:57:50 +0545

    imroved routing

--- client/src/layouts/RootLayout/RootLayout.tsx
+++ client/src/layouts/RootLayout/RootLayout.tsx
@@ -1,16 +1,81 @@
-import React, { useEffect } from "react";
-import Navbar from "../../components/Navbar/Navbar";
-import { Outlet, useNavigate } from "react-router-dom";
+import React, { Suspense, useEffect, useMemo } from "react";
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { fetchUserData } from "../../redux/actions/noteActions";
+import Login from "../../pages/LogIn/LogIn";
+import SignUp from "../../pages/SignUp/SignUp";
+import Homepage from "../../pages/Homepage/Homepage";
+import LoadingSection from "../../components/LoadingScreen/LoadingSection";
+
+const useAuth = () => {
+  const username = useSelector((state: any) => state.userName);
+
+  const isLoggedIn = !!username;
+  const isFailed = username === "";
+  const isLoading = username === null;
+
+  useEffect(() => {
+    const fetchAndLoadUser = async () => {
+      await fetchUserData();
+    };
+
+    fetchAndLoadUser();
+  }, []);
+
+  return { isLoggedIn, isLoading, isFailed };
+};
 
 function RootLayout() {
-    return (
-        <main className="bg-mesh-bright bg-cover min-h-screen">
-            <Navbar />
-            <Outlet />
-        </main>
-    );
+  const { isLoggedIn, isLoading, isFailed } = useAuth();
+
+  const router = useMemo(
+    () =>
+      createBrowserRouter([
+        {
+          path: "/",
+          element: isLoggedIn ? (
+            isLoading ? (
+              <>Loading</>
+            ) : (
+              <Homepage />
+            )
+          ) : (
+            <Navigate to="/users/signin" />
+          ),
+        },
+        {
+          path: "users/signin",
+          element: isLoggedIn ? (
+            <Navigate to="/" />
+          ) : isLoading ? (
+            <LoadingSection />
+          ) : (
+            <Login />
+          ),
+        },
+        {
+          path: "users/signup",
+          element: isLoggedIn ? (
+            <Navigate to="/" />
+          ) : isLoading ? (
+            <LoadingSection />
+          ) : (
+            <SignUp />
+          ),
+        },
+        {
+          path: "*",
+          element: isLoggedIn ? <Navigate to="/" /> : <Navigate to="/users/signin" />,
+        },
+      ]),
+    [isLoggedIn, isLoading]
+  );
+
+  return (
+    <main className="min-h-screen bg-white bg-cover">
+      <RouterProvider router={router} />
+    </main>
+  );
 }
 
 export default RootLayout;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,37 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import SignUp from "./pages/SignUp/SignUp";
-import Homepage from "./pages/Homepage/Homepage";
-import ErrorPage from "./pages/ErrorPage/ErrorPage";
 import { Provider } from "react-redux";
-import "./redux/store";
+import "./redux/store"
 import { store } from "./redux/store";
-import Login from "./pages/LogIn/LogIn";
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Homepage />,
-    },
-    {
-        path: "users/signin",
-        element: <Login />,
-    },
-    {
-        path: "users/signup",
-        element: <SignUp />,
-    },
-    {
-        path: "*",
-        element: <ErrorPage />,
-    },
-]);
+import RootLayout from "./layouts/RootLayout/RootLayout";
 
 const rootElement = document.getElementById("root");
 ReactDOM.createRoot(rootElement!).render(
-    <Provider store={store}>
-        <RouterProvider router={router} />
-    </Provider>
+  <Provider store={store}>
+    <RootLayout />
+  </Provider>
 );
