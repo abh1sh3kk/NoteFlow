47f742e94cf43d73e1d5f57a7145b2a68f55b0a7
abh1sh3kk <abhishekacharya171@gmail.com>
2024-08-18 08:58:39 +0545

    update UI of notes

--- client/src/components/Note/Note.tsx
+++ client/src/components/Note/Note.tsx
@@ -1,21 +1,22 @@
-import React from "react";
+import React, { SyntheticEvent } from "react";
 import { MdOutlineControlPointDuplicate, MdDeleteForever } from "react-icons/md";
 import { BsPinAngle, BsPinAngleFill } from "react-icons/bs";
 import { FiEdit } from "react-icons/fi";
 import { addNote, removeNote } from "../../redux/actions/noteActions";
 
 function Note({ id, title, note, color, dateCreated, dateModified, handleNoteClick }) {
-    const isPinned = false;
-
-    const handleNoteDelete = () => {
+    const handleNoteDelete = (e: SyntheticEvent) => {
+        e.stopPropagation();
         removeNote(id);
     };
 
-    const handleNoteEdit = () => {
+    const handleNoteEdit = (e: SyntheticEvent) => {
+        e.stopPropagation();
         handleNoteClick({ id, title, note, color, dateCreated, dateModified });
     };
 
-    const handleDuplicateNote = () => {
+    const handleDuplicateNote = (e: SyntheticEvent) => {
+        e.stopPropagation();
         const colors = ["#FFF6C7", "#DED9FF", "#DDFFE9", "#FFD9EB", "#D9E8FF"];
         let randomColor = colors[Math.floor(Math.random() * (colors.length - 1))];
         addNote(title, note, randomColor);
@@ -24,16 +25,17 @@ function Note({ id, title, note, color, dateCreated, dateModified, handleNoteCli
     return (
         <section
             style={{ backgroundColor: color }}
-            className="w-full px-6 py-6 text-sm flex flex-col justify-between gap-8 rounded-[10px] hover:shadow-sm cursor-pointer"
+            className="transition-all duration-200 border-[1px] hover:scale-[101%] border-slate-400 w-full px-6 py-6 text-sm flex flex-col justify-between gap-8 rounded-[10px] hover:shadow-sm hover:border-slate-900 cursor-pointer"
+            onClick={handleNoteEdit}
         >
-            <div className="note-upper flex flex-col gap-4 justify-between h-full">
+            <div className="flex flex-col gap-4 justify-between h-full note-upper">
                 <div className="flex flex-col gap-1 sm:gap-2">
-                    <h2 className="text-lg line-clamp-1">{title}</h2>
-                    <p  className="line-clamp-4 text-base">{note}</p>
+                    <h2 className="text-xl font-medium text-slate-800 line-clamp-1" title={title}>{title}</h2>
+                    <p className="text-base font-light line-clamp-4">{note}</p>
                 </div>
                 <div className="flex flex-col gap-1">
                     {!dateModified && (
-                        <p className="line-clamp-2 text-slate-700 ">Created On: {dateCreated}</p>
+                        <p className="line-clamp-2 text-slate-700">Created On: {dateCreated}</p>
                     )}
                     {dateModified && (
                         <p className="line-clamp-2 text-slate-600">Last Modified: {dateModified}</p>
@@ -41,9 +43,8 @@ function Note({ id, title, note, color, dateCreated, dateModified, handleNoteCli
                 </div>
             </div>
 
-            <div className="note-lower flex justify-between gap-2 w-full">
-                <div className="note__icons-left flex gap-4 text-lg">
-                    {/* {isPinned ? <BsPinAngleFill /> : <BsPinAngle />} */}
+            <div className="flex gap-2 justify-between w-full note-lower">
+                <div className="flex gap-4 text-lg note__icons-left">
                     <MdOutlineControlPointDuplicate
                         className="hover:scale-110"
                         onClick={handleDuplicateNote}
@@ -52,7 +53,7 @@ function Note({ id, title, note, color, dateCreated, dateModified, handleNoteCli
 
                     {/* <GoDuplicate /> */}
                 </div>
-                <div onClick={handleNoteDelete} className="flex gap-2 note__icons-right text-xl">
+                <div onClick={handleNoteDelete} className="flex gap-2 text-xl note__icons-right">
                     <MdDeleteForever className="hover:scale-110" />
                 </div>
             </div>
